7593becf97e67bdaefdaaa8a281be1cb8ac7db9e
guccisquad <unknowneg@yandex.ru>
2018-02-10 14:23:56 +0300

    validate fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,43 @@
-$('.btn').click(function(e) {
+$('.form-group textarea').on('keydown',function(e) {
+  if (e.keyCode != 13)
+  return;
   var buttonEnter = $('.btn'),
-   message = $('.user_message').val(),
-   newMessage = "",
-   nickName = $('.user_nickname').val(),
-   newNick = "",
-   nowT = moment().format('HH:mm');
+    message = $('.user_message').val(),
+    newMessage = "",
+    nickName = $('.user_nickname').val(),
+    newNick = "",
+    nowT = moment().format('HH:mm');
 
-   replaceMessage = function() {
-     newMessage = message;
-     var chatBlock = $('.text_area');
-     if (newMessage.length > 0) {
-       var newChatBlock =
-       '<div class="chatMain">' +
-       '<div class="chatNickName">' +
-       '<p>' +nickName+ '</p>'+
-       '</div>' +
-       '<div class="chattext">'+
-       '<p>'+newMessage+'</p>'+
-       '</div>' +
-       '<div class="chatTime">' +
-       '<p>' +nowT+ '</p>' +
-       '</div>'+
-       '</div>';
-       chatBlock.append(newChatBlock);
-     }
+  typingMessage = function() {
+    newMessage = message;
+      var chatBlock = $('.text_area');
+        if (newMessage != 0 && nickName != 0) {
+          var newChatBlock =
+            '<div class="chatMain mt-2">' +
+            '<div class="chatNickName">' +
+            '<p>' +nickName+ '</p>'+
+            '</div>' +
+            '<div class="chattext">'+
+            '<p>'+newMessage+'</p>'+
+            '</div>' +
+            '<div class="chatTime">' +
+            '<p>' +nowT+ '</p>' +
+            '</div>'+
+            '</div>';
+              chatBlock.append(newChatBlock);
+          }
    }
 
-   scrollingTextArea = function() {
-     var chatResult = $('.text_area');
-     chatResult.scrollTop(chatResult.prop('scrollHeight'));
-   }
+  scrollingTextArea = function() {
+    var chatResult = $('.text_area');
+    chatResult.scrollTop(chatResult.prop('scrollHeight'));
+  }
 
-   clearingTextArea = function() {
-     $('.user_message').val("");
-   }
+  clearingTextArea = function() {
+    $('.user_message').val("");
+  }
 
- replaceMessage();
- clearingTextArea();
- scrollingTextArea();
+  typingMessage();
+  scrollingTextArea();
+  clearingTextArea();
 });
