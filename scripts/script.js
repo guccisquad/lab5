0c4241bfa9808fea5c06c908efc436302c028c43
guccisquad <unknowneg@yandex.ru>
2018-02-14 20:05:23 +0300

    localStorage and registration

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,63 @@
+$(".nick_enter").click(function() {
+  var nickName = $('.user_nickname').val();
+  if (nickName != 0) {
+    $('.type_nickname').hide();
+    $('.col-lg-4').show();
+  }
+});
+
 $('.btn').click(function() {
   var message = $('.user_message').val(),
     newMessage = "",
     nickName = $('.user_nickname').val(),
-    newNick = "",
-    nowT = moment().format('HH:mm'),
+    nowT = moment().format('HH:mm');
+
+  clearingTextArea = function() {
+    $('textarea').val('');
+  }
 
   typingMessage = function() {
     var history = {
-      name : nickName,
-      text : message,
-      time : nowT
-    };
-    localStorage.setItem('history', JSON.stringify(history));
-   }
+       name : nickName,
+       text : message,
+       time : nowT
+     };
+     localStorage.setItem('history', JSON.stringify(history));
+     var  historyName = "",
+       historyText = "",
+       historyTime = "",
+       historyName = (JSON.parse(localStorage.getItem('history')).name),
+       historyText = (JSON.parse(localStorage.getItem('history')).text),
+       historyTime = (JSON.parse(localStorage.getItem('history')).time);
+
+       var chatBlock = $('.me');
+       if (historyText != 0) {
+       var newChatBlock =
+         '<div class="chatMain mt-2">' +
+         '<div class="chatNickName">' +
+         '<p>' +historyName+ '</p>'+
+         '</div>' +
+         '<div class="mychattext">'+
+         '<p>'+historyText+'</p>'+
+         '</div>' +
+         '<div class="chatTime">' +
+         '<p>' +historyTime+ '</p>' +
+         '</div>'+
+         '</div>';
+           chatBlock.append(newChatBlock);
+        }
+  }
 
   scrollingTextArea = function() {
     var chatResult = $('.text_area');
     chatResult.scrollTop(chatResult.prop('scrollHeight'));
   }
 
-  clearingTextArea = function() {
-    $('textarea').val("");
-  }
-
+  clearingTextArea();
   typingMessage();
   scrollingTextArea();
-  clearingTextArea();
+
+
 });
 
 
@@ -35,30 +67,28 @@ $('textarea').on('keydown',function(e) {
   }
 });
 
-$(document).ready(function() {
-var  historyName = "",
-  historyText = "",
-  historyTime = "",
-  historyName = (JSON.parse(localStorage.getItem('history'))),
-  historyName = (JSON.parse(localStorage.getItem('history')).name),
-  historyText = (JSON.parse(localStorage.getItem('history')).text),
-  historyTime = (JSON.parse(localStorage.getItem('history')).time);
-  console.log(historyName);
+$(window).bind('storage', function (e) {
+  var  historyName = "",
+    historyText = "",
+    historyTime = "",
+    historyName = (JSON.parse(localStorage.getItem('history')).name),
+    historyText = (JSON.parse(localStorage.getItem('history')).text),
+    historyTime = (JSON.parse(localStorage.getItem('history')).time);
 
-  var chatBlock = $('.text_area');
-    if (historyText != 0 && historyName != 0) {
-      var newChatBlock =
-        '<div class="chatMain mt-2">' +
-        '<div class="chatNickName">' +
-        '<p>' +historyName+ '</p>'+
-        '</div>' +
-        '<div class="chattext">'+
-        '<p>'+historyText+'</p>'+
-        '</div>' +
-        '<div class="chatTime">' +
-        '<p>' +historyTime+ '</p>' +
-        '</div>'+
-        '</div>';
-          chatBlock.append(newChatBlock);
+    var chatBlock = $('.you');
+    if (historyText != 0) {
+    var newChatBlock =
+      '<div class="chatMain mt-2">' +
+      '<div class="chatNickName">' +
+      '<p>' +historyName+ '</p>'+
+      '</div>' +
+      '<div class="chattext">'+
+      '<p>'+historyText+'</p>'+
+      '</div>' +
+      '<div class="chatTime">' +
+      '<p>' +historyTime+ '</p>' +
+      '</div>'+
+      '</div>';
+        chatBlock.append(newChatBlock);
       }
 });
